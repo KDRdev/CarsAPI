9eafa3bcfeacd49bbb79c1cc247b006b7b6bdd32
dkdev <dmitry.kashuba.cv@gmail.com>
2020-08-27 10:54:03 +0200

    Added Procfile, updated requirements.txt

--- cars_api/settings.py
+++ cars_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -112,3 +113,14 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
